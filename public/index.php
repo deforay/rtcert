2bcde46f00c42d82dd49cd16b06de9de9e61d710
Amit Dugar <amit@deforay.com>
2024-04-03 14:23:27 +0530

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
- /**
+
+/**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-
-
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
@@ -17,7 +16,16 @@ if (php_sapi_name() === 'cli-server') {
 }
 
 // Setup autoloading
-require 'init_autoloader.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 // Run the application!
-Laminas\Mvc\Application::init(require 'config/application.config.php')->run();
+//Laminas\Mvc\Application::init(require 'config/application.config.php')->run();
+
+// Config
+$appConfig = include 'config/application.config.php';
+if (file_exists('config/development.config.php')) {
+    $appConfig = Laminas\Stdlib\ArrayUtils::merge($appConfig, include 'config/development.config.php');
+}
+
+// Run the application!
+Laminas\Mvc\Application::init($appConfig)->run();
