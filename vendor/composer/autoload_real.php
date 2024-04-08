b301082b1ee14590e6ce0bdf789eaed01091b36e
brindha <brindha@deforay.com>
2024-04-08 16:07:56 +0530

    Comment out lucidahandwriting in pdf

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitb10e5a0b197a1158dfec9135e14a0356
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitb10e5a0b197a1158dfec9135e14a0356::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+        $requireFile = static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        }, null, null);
+        };
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
